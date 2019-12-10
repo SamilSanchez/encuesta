a4bf2565174bcfa55cc9e1af3961a23d6bcd3c37
Samil Sanchez <ssanchez@WSROMINF009.bellbank.com>
2019-12-10 14:25:57 -0400

    create view votes

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 
 # Django
-from django.shortcuts import render
-from django.http import HttpResponse, Http404 
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect 
+from django.urls import reverse
 
 # Models
 from .models import Question
@@ -14,11 +15,22 @@ def detail(request, question_id):
     return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
 
 def index(request):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
